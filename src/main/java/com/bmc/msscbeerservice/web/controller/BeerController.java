5dac9425d1e00e0aef7b47b679a30039b2172f01
’YeganehNobakht’ <m.yeganeh.n1994@gmail.com>
2023-04-30 11:59:30 +0330

    adding beer controller

---
+++ src/main/java/com/bmc/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,38 @@
+package com.bmc.msscbeerservice.web.controller;
+
+import com.bmc.msscbeerservice.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+/**
+ * @author yeganeh
+ * @created 30/04/2023 - 11:52 AM
+ */
+@RestController
+@RequestMapping("/api/b1/beer")
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId){
+
+        //todo: impl
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveBeer(@RequestBody BeerDto beerDto){
+
+        //todo: impl
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto){
+
+        //todo: impl
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
