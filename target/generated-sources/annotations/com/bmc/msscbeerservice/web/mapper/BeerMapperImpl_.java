ab65524c67f94bd6e727274fedc59ef021dd0a5d
yeganehNobakht <m.yeganeh.n1994@gmail.com>
2023-06-27 10:22:59 +0330

    enable eureka client

--- target/generated-sources/annotations/com/bmc/msscbeerservice/web/mapper/BeerMapperImpl_.java
+++ target/generated-sources/annotations/com/bmc/msscbeerservice/web/mapper/BeerMapperImpl_.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-05-30T10:49:11+0330",
+    date = "2023-06-27T10:12:10+0330",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.14 (Oracle Corporation)"
 )
 @Component
