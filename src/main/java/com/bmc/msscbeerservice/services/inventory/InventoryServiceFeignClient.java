7d73669df8d91e4e24be4311e0e407b794d7c56b
yeganehNobakht <m.yeganeh.n1994@gmail.com>
2023-07-09 14:32:26 +0330

    Configure HTTP Basic Auth for Feign Client

--- src/main/java/com/bmc/msscbeerservice/services/inventory/InventoryServiceFeignClient.java
+++ src/main/java/com/bmc/msscbeerservice/services/inventory/InventoryServiceFeignClient.java
@@ -1,5 +1,6 @@
 package com.bmc.msscbeerservice.services.inventory;
 
+import com.bmc.msscbeerservice.config.FeignClientConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +13,7 @@ import java.util.UUID;
  * @author Masoumeh Yeganeh
  * @created 27/06/2023
  */
-@FeignClient(name = "inventory-service")
+@FeignClient(name = "inventory-service",configuration = FeignClientConfig.class)
 public interface InventoryServiceFeignClient {
 
     @GetMapping(BeerInventoryRestTemplateServiceImpl.INVENTORY_PATH)
